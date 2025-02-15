6625cd8ba843675715c69482b1ee5a32646694b0
Awinel <benjamin.huerta.awi@gmail.com>
2025-02-15 03:41:54 -0300

    feat(dashboard): Navigation between pages
    nav-link.tsx feature to navigate between pages

--- vallenar-city/app/ui/dashboard/nav-links.tsx
+++ vallenar-city/app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import Link from "next/link";
 import { nunito } from "../fonts";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 const links = [
   {
@@ -28,17 +33,24 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <div className="flex flex-wrap md:inline">
       {links.map((link) => {
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className={`${nunito.className} flex h-[48px] grow items-center justify-center rounded-md bg-amber-200 p-3 text-sm font-medium hover:bg-[var(--color-05)] hover:text-[var(--black)] my-1 mx-1 md:mx-0 md:flex-none md:justify-start md:p-2 md:px-3`}
+            className={clsx(
+              `${nunito.className} flex h-[48px] grow items-center justify-center rounded-md bg-amber-200 p-3 text-sm font-medium hover:bg-[var(--color-05)] hover:text-[var(--black)] my-1 mx-1 md:mx-0 md:flex-none md:justify-start md:p-2 md:px-3`,
+              {
+                "bg-amber-400": pathname === link.href,
+              }
+            )}
           >
             <p className="p-2 m-1]">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </div>
