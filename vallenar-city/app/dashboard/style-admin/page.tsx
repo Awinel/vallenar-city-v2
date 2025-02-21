d129a49ac9b2dfbf9d01a8722c7ecb019d2498b5
Awinel <benjamin.huerta.awi@gmail.com>
2025-02-21 12:25:08 -0300

    display update

--- vallenar-city/app/dashboard/style-admin/page.tsx
+++ vallenar-city/app/dashboard/style-admin/page.tsx
@@ -1,3 +1,30 @@
-export default function Page() {
-  return <p>Style Page</p>;
+import { fetchStyle } from "@/app/lib/data";
+import StyleDisplayCard from "@/app/ui/style/StyleDisplayCard";
+
+export default async function StylePage() {
+  const styles = await fetchStyle();
+
+  return (
+    <div>
+      <h1 className={`text-2xl text-center`}>Style</h1>
+      <div className="flex ">
+        {styles.map((style) => (
+          <StyleDisplayCard
+            key={style.id}
+            id={style.id}
+            name={style.name}
+            address={style.address}
+            addressnumber={style.addressnumber}
+            description={style.description}
+            image={style.image}
+            phone={style.phone}
+            type={style.type}
+            email={style.email}
+            facebook={style.facebook}
+            instagram={style.instagram}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
