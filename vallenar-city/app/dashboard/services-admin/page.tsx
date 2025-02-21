6fbbdcd0d45068b1982083888fa7bfba92d06b14
Awinel <benjamin.huerta.awi@gmail.com>
2025-02-21 12:34:28 -0300

    Service display card update

--- vallenar-city/app/dashboard/services-admin/page.tsx
+++ vallenar-city/app/dashboard/services-admin/page.tsx
@@ -1,3 +1,30 @@
-export default function Page() {
-  return <p>Services Page</p>;
+import { fetchServices } from "@/app/lib/data";
+import ServiceDisplayCard from "@/app/ui/services/ServiceDisplayCard";
+
+export default async function StylePage() {
+  const styles = await fetchServices();
+
+  return (
+    <div>
+      <h1 className={`text-2xl text-center`}>Services</h1>
+      <div className="flex ">
+        {styles.map((style) => (
+          <ServiceDisplayCard
+            key={style.id}
+            id={style.id}
+            name={style.name}
+            address={style.address}
+            addressnumber={style.addressnumber}
+            description={style.description}
+            image={style.image}
+            phone={style.phone}
+            type={style.type}
+            email={style.email}
+            facebook={style.facebook}
+            instagram={style.instagram}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
